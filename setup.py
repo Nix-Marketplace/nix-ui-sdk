07925f2ac4490e08c1a0885a46304c9fc2c0e58d
Kajetan Tukendorf <34398883+Bacoknight@users.noreply.github.com>
2024-03-15 11:19:34 +0000

    Updated requests dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NixGUI',
+    name='nixgui',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'nicegui',
+        'requests'
     ],
     author='Nix',
     author_email='kay@nix.tech',
